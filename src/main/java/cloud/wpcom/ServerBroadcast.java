f58bfaba6a08c699cd28b3b154221b5d7e8f48ce
Landry <landrystrickland@gmail.com>
2022-01-29 00:13:37 -0600

    added color code compatibility

--- src/main/java/cloud/wpcom/ServerBroadcast.java
+++ src/main/java/cloud/wpcom/ServerBroadcast.java
@@ -4,29 +4,32 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+
 
 public class ServerBroadcast implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] mssg) {
 
-        // Combine all message arguments to a single string
-        String message = WPCraft.PREFIX;
-        for (int i = 0; i < mssg.length; i++) {
-            message += " " + mssg[i];
+        // Concatinate args and check for color codes
+        String message = "";
+        for (String m : mssg) {
+            message += m;
         }
+        message = ChatColor.translateAlternateColorCodes('&', message);
 
         // Send message to online players, capturing number of players sent (including the console)
-        Integer playersSent = sender.getServer().broadcastMessage(message);
+        Integer playersSent = sender.getServer().broadcastMessage(WPCraft.PREFIX + message);
        
         if (sender instanceof Player) { // If the sender is a player
             
-            // Provide confirmation through PM if the sender is a player
+            // Provide confirmation through PM
             ((Player)sender).sendMessage(WPCraft.PREFIX + WPCraft.PM_PREFIX + " Your message was sent to " + (playersSent - 1) + " player(s)");
 
         } else { // Handle other senders, AKA console
 
-            // Provide confirmation to the console
+            // Provide confirmation through the console
             sender.getServer().getLogger().info("[WPCraft] Your message was sent to " + (playersSent - 1) + " player(s)");
         }
         
