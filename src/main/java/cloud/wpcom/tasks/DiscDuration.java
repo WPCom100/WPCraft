afe79d2ee2ae78bf145f47a77e500b481f0e6569
Landry <landrystrickland@gmail.com>
2022-02-11 03:27:38 -0600

    added more jukebox hopper functionality

--- src/main/java/cloud/wpcom/tasks/DiscDuration.java
+++ src/main/java/cloud/wpcom/tasks/DiscDuration.java
@@ -18,24 +18,43 @@ public class DiscDuration extends BukkitRunnable {
     
     // Runs at the supposed end of the playing disc
     @Override
-    public void run() {
+    public void run() { //TODO HANDLE IF HOPPER IS FULL
         jukeboxw.setPlaying(false);
 
         // If the Jukebox has an output hopper, place the current playing disc into it
         if (jukeboxw.hasOutputHopper()) {
-            jukeboxw.getInputHopperInventory().addItem(new ItemStack(jukeboxw.getBlock().getPlaying()));
-            //TODO HANDLE IF HOPPER IS FULL
+            WPCraft.server
+                    .broadcastMessage("Output Disc to Output Hopper " + jukeboxw.getBlock().getPlaying().toString());
+            jukeboxw.getOutputHopperInventory().addItem(new ItemStack(jukeboxw.getBlock().getPlaying()));
             jukeboxw.getBlock().setRecord(new ItemStack(Material.AIR));
+            jukeboxw.getBlock().update();
         }
-
-        // If a disc is waiting in the input hopper, play it next
-        if (jukeboxw.hasWaitingDisc() != -1)
-            jukeboxw.playRecord(jukeboxw.popWaitingDisc(), plugin);
+        playNext();
     }
-
+    
+    // Runs when a player cancles the playing of a disc manuely
     @Override
     public void cancel() {
-        super.cancel(); // TODO MIGHT NOT BE NEEDED
+        WPCraft.server.broadcastMessage("Duration task cancled");
+        jukeboxw.setPlaying(false);
+        super.cancel();
+        
+        // Schedule next disc to play after disc is ejected in next tick
+        BukkitRunnable playTask = new BukkitRunnable() {
+            @Override
+            public void run() {
+                playNext();
+            }
+        };
+        playTask.runTask(plugin);
     }
 
+    // If a disc is waiting in the input hopper, play it next
+    public void playNext() {
+        int waitingDiscIndex = jukeboxw.getWaitingDisc();
+        if (waitingDiscIndex != -1) {
+            WPCraft.server.broadcastMessage("Jukebox has a waiting disk at: " + waitingDiscIndex);
+            jukeboxw.playRecord(jukeboxw.popInputHopperAtIndex(waitingDiscIndex), plugin);
+        }
+    }
 }
